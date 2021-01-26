454da783cd54edf094d8838c0d992b85878ee1c7
Patel Neel Mahesh <77996149+PatelNeelMahesh@users.noreply.github.com>
2021-01-26 23:50:10 +0530

    Add files via upload

---
+++ N_0_Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class N_0_Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("Sum of the number is: ");
+        int num1 = 8;
+        int num2 = 9;
+        int sum;
+        sum=num1+num2;
+        System.out.println(sum);
+
+    }
+}
