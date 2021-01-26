6672e2a6f4b84fd68e3344352716b75708108dda
Patel Neel Mahesh <77996149+PatelNeelMahesh@users.noreply.github.com>
2021-01-27 01:21:48 +0530

    Add files via upload

---
+++ N_8_String_Methods.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class N_8_String_Methods {
+    public static void main(String[] args) {
+        String name = "Abcde";
+        // System.out.println(name);
+        int value = name.length();
+        //System.out.println(value);
+
+        //String lstring = name.toLowerCase();
+        //System.out.println(lstring);
+
+        //String ustring = name.toUpperCase();
+        //System.out.println(ustring);
+
+        //String nonTrimmedString = "    AbAsd    ";
+        //System.out.println(nonTrimmedString);
+
+        //String trimmedString = nonTrimmedString.trim();
+        //System.out.println(trimmedString);
+
+        //System.out.println(name.substring(1));
+        //System.out.println(name.substring(1,5));
+
+        //System.out.println(name.replace('b', 'p'));
+        //System.out.println(name.replace("A", "sasfd"));
+
+        //System.out.println(name.startsWith("A"));
+        //System.out.println(name.endsWith("lk"));
+
+        //System.out.println(name.charAt(4));
+
+        //String modifiedName = "Abcggsggs";
+        //System.out.println(modifiedName.indexOf("ggs"));
+        //System.out.println(modifiedName.indexOf("ggs", 4));
+        //System.out.println(modifiedName.lastIndexOf("ggs", 7));
+
+        //System.out.println(name.equals("Abcde"));
+        System.out.println(name.equalsIgnoreCase("AbcdE"));
+
+        System.out.println("I am escape sequence\tdouble quote");
+
+
+
+
+    }
+}
