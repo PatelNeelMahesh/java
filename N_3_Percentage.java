7d4e610ced683aa8f091cbde4127ea8363ad0aac
Patel Neel Mahesh <77996149+PatelNeelMahesh@users.noreply.github.com>
2021-01-26 23:51:18 +0530

    Add files via upload

---
+++ N_3_Percentage.java
@@ -0,0 +1,36 @@
+package com.company;
+import java.util.Scanner;
+
+public class N_3_Percentage {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your marks : ");
+
+        System.out.println("Subject 1: ");
+        float subOne = sc.nextFloat();
+
+        System.out.println("Subject 2: ");
+        float subTwo = sc.nextFloat();
+
+        System.out.println("Subject 3: ");
+        float subThree = sc.nextFloat();
+
+        System.out.println("Subject 4: ");
+        float subFour = sc.nextFloat();
+
+        System.out.println("Subject 5: ");
+        float subFive = sc.nextFloat();
+
+        float totalMarks = subOne + subTwo + subThree + subFour + subFive ;
+
+        float studentPercentage = totalMarks/5.0f;
+
+        System.out.println("Total marks out of 500 : ");
+        System.out.println(totalMarks);
+
+        System.out.println("Student's Percentage :");
+        System.out.println(studentPercentage);
+    }
+}
