1c721c3c7856ffbdcdd98e2a985c990e9767f27c
Patel Neel Mahesh <77996149+PatelNeelMahesh@users.noreply.github.com>
2021-01-26 23:50:58 +0530

    Add files via upload

---
+++ N_2_TakingInput.java
@@ -0,0 +1,26 @@
+package com.company;
+import java.util.Scanner;
+
+public class N_2_TakingInput {
+    public static void main(String[] args)
+    {
+        Scanner var = new Scanner(System.in);
+
+        System.out.print("Enter num1: ");
+        int num1 = var.nextInt();
+
+        System.out.print("Enter num2: ");
+        int num2 = var.nextInt();
+
+        System.out.println("Enter your name: ");
+        String name = var.next();
+        System.out.println(name);
+
+        int total = num1+num2;
+
+        System.out.print("Total of num1 & num2 is : ");
+
+        System.out.println(total);
+
+    }
+}
