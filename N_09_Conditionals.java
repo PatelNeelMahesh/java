5ef2ecc9b6cc5bb7df816f914d8ac27f15b44c23
Patel Neel Mahesh <77996149+PatelNeelMahesh@users.noreply.github.com>
2021-01-28 18:26:43 +0530

    Update and rename N_9_Conditionals.java to N_09_Conditionals.java

--- N_09_Conditionals.java
+++ N_09_Conditionals.java
@@ -1,7 +1,7 @@
 package com.company;
 import java.util.Scanner;
 
-public class N_9_Conditionals {
+public class N_09_Conditionals {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
